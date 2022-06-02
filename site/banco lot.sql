624543d954db3be52e5fd9c942260da141ae193a
thaylaandreassi <thayla.diogenes@sptech.school>
2022-06-02 09:52:32 -0300

    Alterações na pagina e banco

--- site/banco lot.sql
+++ site/banco lot.sql
@@ -4,9 +4,18 @@ USE lot;
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+	nome VARCHAR(45),
+	email VARCHAR(45),
+	senha VARCHAR(45)
+);
+
+CREATE TABLE aviso(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 );
 
 select * from usuario;
+select * from aviso;
--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,7 +21,7 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "lot",
-    user: "root",
+    user: "aluno",
     password: "sptech",
 };
 
